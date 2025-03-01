a0db7f8691b3d144955a34491fdf9a6c300208ed
pavanjvm <pavanprasad05062gmail.com>
2025-03-01 12:18:06 +0530

    .

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 echo "Installing dependencies..."
-cd server
 npm install
--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Unzipping server.zip..."
-cd ..
 unzip -o server.zip
 rm -f server.zip  # Remove zip after extracting # Ensure scripts are executable
 echo "Unzip complete."
