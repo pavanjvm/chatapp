f7f4cbc30b2e743d71b71801fc021f7eae325c8e
pavanjvm <pavanprasad05062gmail.com>
2025-03-01 12:49:57 +0530

    ,

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
 // Load environment variables FIRST
-dotenv.config({ path: "./server/.env" });
+dotenv.config({ path: ".env" });
 
 if (!process.env.MONGO_URI) {
   console.error("Error: MONGO_URI is not defined. Check your .env file!");
@@ -38,40 +38,3 @@ const server = app.listen(PORT, () => {
 });
 
 // WebSocket (Socket.io)
-const io = require("socket.io")(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: "http://localhost:3000",
-  },
-});
-
-io.on("connection", (socket) => {
-  //console.log("Connected to Socket.io !!");
-
-  socket.on("setup", (userData) => {
-    socket.join(userData._id);
-    socket.emit("connected");
-  });
-
-  socket.on("join chat", (room) => {
-    socket.join(room);
-    console.log("Room joined: " + room);
-  });
-
-  socket.on("typing", (room) => socket.in(room).emit("typing"));
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-
-  socket.on("new message", (newMessageRecieved) => {
-    let chat = newMessageRecieved.chat;
-    if (!chat.users) return console.log("chat.users not found !!");
-    chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return;
-      socket.in(user._id).emit("message received", newMessageRecieved);
-    });
-  });
-
-  socket.off("setup", () => {
-    console.log("USER DISCONNECTED !");
-    socket.leave(userData._id);
-  });
-});
---
+++ server/scripts/stop-server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Unzipping server.zip..."
+
