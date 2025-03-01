37c70b81089beaf6d14541fc2a230b321cc91a26
pavanjvm <pavanprasad05062gmail.com>
2025-03-01 11:39:46 +0530

    .

---
+++ scripts/stop-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Unzipping server.zip..."
+cd /app
+unzip -o server.zip
+rm -f server.zip  # Remove zip after extracting
+chmod +x server/scripts/*.sh  # Ensure scripts are executable
+echo "Unzip complete."
