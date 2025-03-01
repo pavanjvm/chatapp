b2221c026f3d27b2d92fc5589ae7a42b67b117e6
pavan prasad <137184666+pavanjvm@users.noreply.github.com>
2025-03-01 18:05:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(errorHandler);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server started on PORT ${PORT}!!`);
 });
 
